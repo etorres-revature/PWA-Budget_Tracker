432adfc750058ea8ed7098fa03d48ceb4364e4d6
Eric D. Torres <etorresnotary@gmail.com>
2020-11-19 08:23:20 -0600

    add workbox plugin for webpack to generate caceing service worker

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,41 @@
-const path = require('path');
+const path = require("path");
+
+const { GenerateSw } = require("workbox-webpack-plugin");
 
 /**
  * Webpack Config
  * @see https://webpack.js.org/concepts/configuration/
  */
 const config = {
-    entry: "./public/js/index.js",
-    output: {
-        path: path.resolve(__dirname, "/public/dist"),
-        filename: "bundle.js"
-    },
-    mode: "development",
-    plugins: [
-        new WebpackPwaManifest({
-            publicPath: "/dist/",
-            filename: "manifest.webmanifest",
-            inject: false,
-            name: "Progressive Web Application Budget Tracker",
-            short_name: "PWA Budget Tracker App",
-            theme_color: "#ffffff",
-            background_color: "#ffffff",
-            start_url: "/",
-            display: "standalone",
-            icons: [
-                {
-                    src: path.resolve(__dirname, "public/icons/icon-512x512.png"), 
-                    size: [72, 96, 128, 144, 152, 192, 384, 512]
-                }
-            ]
-        })
-    ]
+  entry: "./public/js/index.js",
+  output: {
+    path: path.resolve(__dirname, "/public/dist"),
+    filename: "bundle.js",
+  },
+  mode: "development",
+  plugins: [
+    new WebpackPwaManifest({
+      publicPath: "/dist/",
+      filename: "manifest.webmanifest",
+      inject: false,
+      name: "Progressive Web Application Budget Tracker",
+      short_name: "PWA Budget Tracker App",
+      theme_color: "#ffffff",
+      background_color: "#ffffff",
+      start_url: "/",
+      display: "standalone",
+      icons: [
+        {
+          src: path.resolve(__dirname, "public/icons/icon-512x512.png"),
+          size: [72, 96, 128, 144, 152, 192, 384, 512],
+        },
+      ],
+    }),
+    new GenerateSw({
+      swSrc: "./public/dist",
+      swDest: "service-worker.js",
+    }),
+  ],
 };
 
 module.exports = config;
