125c94d2b73729554ce517b2a2c78869affd9b6e
Eric D. Torres <etorresnotary@gmail.com>
2020-11-18 23:08:28 -0600

    webpack PWA manifest config added

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,29 @@ const path = require('path');
 const config = {
     entry: "./public/js/index.js",
     output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "/public/dist"),
         filename: "bundle.js"
     },
     mode: "development",
+    plugins: {
+        new WebpackPwaManifest({
+            publicPath: "/dist/",
+            filename: "manifest.json",
+            inject: false,
+            name: "Progressive Web Application Budget Tracker",
+            short_name: "PWA Budget Tracker App",
+            theme_color: "#ffffff",
+            background_color: "#ffffff",
+            start_url: "/",
+            display: "standalone",
+            icons: [
+                {
+                    src: path.resolve(__dirname, "public/icons/icon-512x512.png"), 
+                    size: [72, 96, 128, 144, 152, 192, 384, 512]
+                }
+            ]
+        })
+    }
 };
 
 module.exports = config;
